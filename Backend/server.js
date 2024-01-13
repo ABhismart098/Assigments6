3537d794dd413333f0ec11db4acedd2482199e1e
Abhishek Yadav <abhismart098@gmail.com>
2024-01-13 22:05:19 +0530

    error

--- Backend/server.js
+++ Backend/server.js
@@ -53,11 +53,31 @@
 // app.listen(process.env.PORT || 3001, () => {
 //   console.log(`Server listening at http://localhost:${port}`);
 // });
+// const express = require('express');
+// const connectDB = require('./Config/config'); // Assuming your MongoDB connection setup is in a separate file
+// const yourRoutes = require('./Route/route'); // Adjust the path accordingly
+
+// const app = express();
+
+// // Connect to MongoDB
+// connectDB();
+
+// // Use your routes
+// app.use('/api', yourRoutes); // You can adjust the base path ('/api') according to your needs
+
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`http://localhost:${PORT}`);
+// });
 const express = require('express');
 const connectDB = require('./Config/config'); // Assuming your MongoDB connection setup is in a separate file
 const yourRoutes = require('./Route/route'); // Adjust the path accordingly
 
 const app = express();
+
+// Middleware for parsing JSON and form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
 connectDB();
